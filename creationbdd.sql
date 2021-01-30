3173396939ccc253b0cb9adc2e323ac043d52b42
NathanSalez <nathansalez@hotmail.fr>
2021-01-30 15:20:14 +0100

    ajout insertions dans bdd

--- creationbdd.sql
+++ creationbdd.sql
@@ -71,6 +71,28 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
 
+INSERT INTO `geocache`.`UTILISATEUR`
+(`pseudo`,`description`,`urlPhoto`)
+VALUES
+    ('Naysson', 'Co-fondateur du site avec l\'illustre et puissant Martintin.', 'www.google.com/images'),
+	('Martintin', 'Co-fondateur du site avec le tout-puissant et célèbre Naysson.', 'localhost'),
+    ('RaphaelRowe', 'My name is Raphael Rowe and I have been 12 years behind bars!', ''),
+    ('Sacha', '1000 d\'elo sur chess.com, futur Magnus Carlsen.', '');
+
+INSERT INTO `geocache`.`CACHE`
+    (`description`,`latitude`,`longitude`,`url`,`etat`,`type`,`nature`,`idProprietaire`)
+VALUES
+    ('Description de la première cachette', 50.5015979, 3.1689719, NULL, 'activée', 'traditionnelle', 'physique', 1),
+    ('Description de la deuxième cachette', 50.4351, 2.82432, NULL, 'suspendue', 'traditionnelle', 'physique', 2),
+    ('Description de la troisième cachette', NULL, NULL, 'https://www.youtube.com/', 'fermée', 'traditionnelle', 'virtuelle', 1);
+
+INSERT INTO `geocache`.`VISITE`
+    (`idCache`,`idUtilisateur`,`dateHeure`,`urlPhoto`,`commentaire`,`reussi`)
+VALUES
+    (1,1,'2020-01-01 19:28','www.google.com','Test balise 1 Naysson',1),
+    (1,2,'2020-01-01 19:30','www.google.com','Test balise 2 Martintin',1),
+    (3,3,'2020-01-27 20:01','','I am travelling the world to discover the toughest prisons!',0),
+    (3,1,'2020-02-01 00:00','','Naysson balise 3',1);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
